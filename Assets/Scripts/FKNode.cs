ceedbec656a25e3ab7b1f712a718b82009c983ed
Axel Castells Monllau <axelcastells.dev@gmail.com>
2018-10-24 17:56:28 +0200

    Working on FK

---
+++ Assets/Scripts/FKNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FKNode : MonoBehaviour {
+
+    public float angle;
+    public Vector3 axis;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
