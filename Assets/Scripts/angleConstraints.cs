a59f9912b1518464e7302a0ccad42b09e40b3017
Axel Castells Monllau <axelcastells.dev@gmail.com>
2018-10-24 16:53:13 +0200

    Unity Package for Kinematics imported.

---
+++ Assets/Scripts/angleConstraints.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angleConstraints : MonoBehaviour
+{
+    public bool active;
+
+    [Range(0.0f, 180.0f)]
+    public float maxAngle;
+
+    [Range(0.0f, 180.0f)]
+    public float minAngle;
+
+    public Transform parent;
+    public Transform child;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+           //solve your exercise here
+            
+        }
+    }
+
+    //add auxiliary functions, if needed, below
+
+
+
+
+}
