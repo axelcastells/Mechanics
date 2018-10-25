ff3883931d3a3fefbe7c382ee7e2b6e15603087a
ASUS-DEV\alumno <dj.pez.1997@gmail.com>
2018-10-25 16:39:23 +0200

    Quaternion exercicies

---
+++ Assets/Scripts/AlignmentScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignmentScript : MonoBehaviour
+{
+
+    public Transform target1;
+    public Transform target2;
+    private Quaternion savedOffset;
+	public int exercise = 1;
+	
+	
+    // Use this for initialization
+    void Start ()
+    {
+        savedOffset = target1.rotation * Quaternion.Inverse(transform.rotation);
+    }
+
+    void Update()
+    {
+        switch(exercise)
+        {
+            case 1:
+            {
+
+                    float angleX = -Mathf.Acos(Vector3.Dot(transform.right, target1.right)) * Mathf.Rad2Deg;
+                    Vector3 axisX = Vector3.Cross(transform.right, target1.right).normalized;
+
+                    target1.Rotate(axisX, angleX, Space.World);
+
+
+                    float angleY = -Mathf.Acos(Vector3.Dot(transform.up, target1.up)) * Mathf.Rad2Deg;
+                    Vector3 axisY = Vector3.Cross(transform.up, target1.up).normalized;
+
+                    target1.Rotate(axisY, angleY, Space.World);
+
+
+                }
+                break;
+
+          
+
+            case 2:
+            {
+                    target1.rotation = Quaternion.Slerp(target1.rotation, transform.rotation, 0.01f);
+            } break;
+
+            case 3:
+            {
+                    target1.rotation = transform.rotation * savedOffset;
+            } break;
+
+            case 4:
+            {
+                    target1.rotation = transform.rotation * savedOffset;
+                    target2.rotation = target1.rotation * Quaternion.Inverse(savedOffset);
+            } break;
+        }
+    }
+}
---
+++ Assets/Scripts/MyQuaternionClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MyQuaternionClass : MonoBehaviour {
+
+    public MyQuaternionClass myQuat;
+
+    public float x, y, z, w;
+
+    //Quaternion coordinates
+    public void quatCoord(float x, float y, float z, float w)
+    {
+        myQuat.x = x;
+        myQuat.y = y;
+        myQuat.z = z;
+        myQuat.w = w;
+    }
+
+}
