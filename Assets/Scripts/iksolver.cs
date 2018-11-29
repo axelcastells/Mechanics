1f87738b150d07fa840c2a7942b1fec00d62a77d
Axel Castells Monllau <axelcastells.dev@gmail.com>
2018-11-29 17:58:52 +0100

    Trying to get IKSolver to work

--- Assets/Scripts/iksolver.cs
+++ Assets/Scripts/iksolver.cs
@@ -56,44 +56,57 @@ public class iksolver : MonoBehaviour {
 				// going back up to the root
 				for (int i = joints.Length - 2; i >= 0; i--)
 				{
-					// The vector from the ith joint to the end effector
-					//Vector3 r1 = TODO1
 
-					// The vector from the ith joint to the target
-					//Vector3 r2 = TODO2
-				   
-					// to avoid dividing by tiny numbers
-					/* if (r1.magnitude * r2.magnitude <= 0.001f)
-					{
-						// cos ? sin? 
-                        TODO3
-					}
-					else
-					{
-						// find the components using dot and cross product
-						TODO4
+                    // The vector from the ith joint to the end effector
+                    Vector3 r1 = joints[joints.Length - 1].transform.position - joints[i].transform.position;
+                    Debug.Log("Vector 1 / Iteration: "+i+ ":" + r1);
 
-					}*/
-					
-					// The axis of rotation 
-					//Vector3 axis = TODO5
-				  
-					// find the angle between r1 and r2 (and clamp values if needed avoid errors)
-					//theta[i] = TODO6 
+                    // The vector from the ith joint to the target
+                    Vector3 r2 = targ.transform.position - joints[i].transform.position;
+                    Debug.Log("Vector 2 / Iteration: " + i + ":" + r2);
 
-					//Optional. correct angles if needed, depending on angles invert angle if sin component is negative
-					//if (TODO)
-					//	theta[i] = TODO7
+                    // to avoid dividing by tiny numbers
+                    if (r1.magnitude * r2.magnitude <= 0.001f)
+                    {
+                        
+                        //cos? sin? 
+                        //TODO3
+
+                    }
+                    else
+                    {
+                        //find the components using dot and cross product
+
+                        //TODO4
 
 
+                    }
 
-					// obtain an angle value between -pi and pi, and then convert to degrees
-					//theta[i] = TODO8
+                    // The axis of rotation 
+                    // Vector3 axis = TODO5
+                    Vector3 axis = Vector3.Cross(r1.normalized, r2.normalized);
 
-					// rotate the ith joint along the axis by theta degrees in the world space.
-					// TODO9
+                    // find the angle between r1 and r2 (and clamp values if needed avoid errors)
+                    //theta[i] = TODO6 
+                    float angle = Mathf.Acos(Vector3.Dot(r1.normalized, r2.normalized));
+                    _theta[i] = angle;
 
-				}
+
+                    //Optional. correct angles if needed, depending on angles invert angle if sin component is negative
+                    //	theta[i] = TODO7
+
+                    //if (Mathf.Sin(angle) < 0)
+                    //    angle += 180;
+
+
+                    // obtain an angle value between -pi and pi, and then convert to degrees
+                    //theta[i] = TODO8
+
+                    // rotate the ith joint along the axis by theta degrees in the world space.
+                    // TODO9
+                    joints[i].transform.rotation = Quaternion.AngleAxis(angle, axis);
+
+                }
 				
 				// increment tries
 				_tries++;
